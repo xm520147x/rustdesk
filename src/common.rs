2a86366af7064ee1b691bbce34eeaa73ad6dfca0
root <601772524@qq.com>
2026-06-01 18:09:26 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1081,7 +1081,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://rustapi.cdlixin.com:21114".to_owned()
+    "http://rustapi.cdlixin.com:21114".to_owned()
 }
 
 #[inline]
