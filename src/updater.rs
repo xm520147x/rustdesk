228130626a216d7b2f19d1379f492037bd29586b
root <601772524@qq.com>
2026-06-08 09:59:57 +0800

    修改停止自动更新

--- src/updater.rs
+++ src/updater.rs
@@ -120,13 +120,13 @@ fn start_auto_update_check_(rx_msg: Receiver<UpdateMsg>) {
 }
 
 fn check_update(manually: bool) -> ResultType<()> {
-    #[cfg(target_os = "windows")]
-    let update_msi = crate::platform::is_msi_installed()? && !crate::is_custom_client();
-    if !(manually || config::Config::get_bool_option(config::keys::OPTION_ALLOW_AUTO_UPDATE)) {
-        return Ok(());
-    }
-    if do_check_software_update().is_err() {
-        // ignore
+    //#[cfg(target_os = "windows")]
+    //let update_msi = crate::platform::is_msi_installed()? && !crate::is_custom_client();
+    //if !(manually || config::Config::get_bool_option(config::keys::OPTION_ALLOW_AUTO_UPDATE)) {
+    //    return Ok(());
+    //}
+    //if do_check_software_update().is_err() {
+    // ignore
         return Ok(());
     }
 
